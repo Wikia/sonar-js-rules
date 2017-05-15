d143130a492acf4f774e6ccc93ab46c6037f5e61
TK-999 <tk.999.wikia@gmail.com>
2017-05-15 16:25:30 +0200

    Rebuild against Sonar API 6.2

--- src/main/java/com/wikia/sonarjs/rules/checks/es6/PropertyShorthandCheck.java
+++ src/main/java/com/wikia/sonarjs/rules/checks/es6/PropertyShorthandCheck.java
@@ -1,12 +1,10 @@
 package com.wikia.sonarjs.rules.checks.es6;
 
+import javax.annotation.Nonnull;
 import org.sonar.check.Rule;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
 import org.sonar.plugins.javascript.api.tree.expression.ObjectLiteralTree;
-import org.sonar.plugins.javascript.api.tree.expression.PairPropertyTree;
 import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
-
-import javax.annotation.Nonnull;
 
 @Rule(key = "PropertyShorthands")
 public class PropertyShorthandCheck extends DoubleDispatchVisitorCheck {
@@ -14,16 +12,9 @@ public class PropertyShorthandCheck extends DoubleDispatchVisitorCheck {
 
 	@Override
 	public void visitObjectLiteral(@Nonnull ObjectLiteralTree tree) {
-		tree.properties().forEach(entry -> {
-			if (entry instanceof PairPropertyTree) {
-				PairPropertyTree pairPropertyTree = (PairPropertyTree) entry;
-				if (!(pairPropertyTree.key().is(Kind.STRING_LITERAL, Kind.IDENTIFIER_REFERENCE))) {
-					addIssue(pairPropertyTree, MESSAGE);
-				}
-			} else {
-				addIssue(entry, MESSAGE);
-			}
-		});
+		tree.properties().stream()
+				.filter(property -> !property.is(Kind.PAIR_PROPERTY))
+				.forEach(shortHandProperty -> addIssue(shortHandProperty, MESSAGE));
 
 		super.visitObjectLiteral(tree);
 	}
